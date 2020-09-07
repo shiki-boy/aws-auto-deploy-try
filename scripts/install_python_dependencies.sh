1b92ab1dd5cb4e39172ab7a206c91cdd4bf890e2
raghav-sl <raghav@startuplabs.io>
2020-09-07 13:43:35 +0530

    try

--- scripts/install_python_dependencies.sh
+++ scripts/install_python_dependencies.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 cd /home/ubuntu/code/aws-auto-deploy-try
 pwd
+git remote -v
 git pull
 source /home/ubuntu/code/envs/aws-auto-deploy-try-env/bin/activate
 pip3 install -r requirements.txt
