545aa7da7849eef144c229bc311d392851947332
raghav-sl <raghav@startuplabs.io>
2020-09-08 17:27:32 +0530

    try

--- animal/tests.py
+++ animal/tests.py
@@ -4,7 +4,7 @@ from animal.models import Animal
 class AnimalTestCase(TestCase):
     def setUp(self):
         Animal.objects.create(name="lion", sound="roar")
-        Animal.objects.create(name="cat", sound="quack")
+        Animal.objects.create(name="cat", sound="meow")
 
     def test_animals_can_speak(self):
         """Animals that can speak are correctly identified"""
