ca94ab71ddb0a31575fe28ce6909376b9db37ade
raghav-sl <raghav@startuplabs.io>
2020-09-08 18:20:54 +0530

    test

--- animal/views.py
+++ animal/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def test_view(request,*args, **kwargs):
+    return HttpResponse("<h1 style='text-align:center'>Hello World</h1>")
--- aws_auto_deploy/urls.py
+++ aws_auto_deploy/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from animal.views import test_view
 #change 8
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', test_view, name="home"),
 ]
